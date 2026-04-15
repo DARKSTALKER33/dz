3169e6e7ffc04ea35dfa5f524f17c41a2547790f
DARKSTALKER33 <slava.228.dyakonov@gmail.com>
2026-04-15 14:29:03 +0300

    добавление текста,ввода параметров, реконструкция с помощью функций

--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
-	const USDT = 100
-    
-    var EUR = 0.8643
-	convert1 := float64(USDT) * EUR
-    fmt.Println(convert1)
-    
-	var RUB =  78.35
-	convert2 := float64(USDT) * RUB
-	fmt.Println(convert2)
+func main() {
 
-	convert3 := convert2 / convert1
-	fmt.Println(convert3)
+	USDT, EUR, RUB := getUserInput()
+
+	var eurToUsdt float64 = 1.08
+	var rubToUsdt float64 = 0.01
+
+	convert1, convert2, convert3 := convertation(USDT, EUR, RUB, eurToUsdt, rubToUsdt )
+	fmt.Printf("USDT к EUR: %.2f EUR\n", convert1)
+	fmt.Printf("USDT к RUB: %.2f RUB\n", convert2)
+	fmt.Printf("EUR к RUB: %.2f RUB\n", convert3)
+}
+
+func getUserInput() (float64, float64, float64) {
+	var USDT, EUR, RUB float64
+
+	fmt.Println("Введите количество USDT")
+	fmt.Scan(&USDT)
+	fmt.Println("Введите количество EUR")
+	fmt.Scan(&EUR)
+	fmt.Println("Введите количество RUB")
+	fmt.Scan(&RUB)
+	return USDT, EUR, RUB
+}
+
+func convertation(USDT, EUR, RUB, eurToUsdt, rubToUsdt float64) (float64, float64, float64) {
+	convert1 := USDT / eurToUsdt
+	convert2 := USDT / rubToUsdt
+	eurInUsdt := EUR * eurToUsdt
+	convert3 := eurInUsdt / rubToUsdt
+	return convert1, convert2, convert3
+
 }
